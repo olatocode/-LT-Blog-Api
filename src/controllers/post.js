7473534dc088fbaee96acc99d4deba926f85058c
olatocode <awosolat@gmail.com>
2025-03-24 12:30:25 +0100

    feat: Add getApost and updatepost

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,25 +1,71 @@
 /** @format */
 
 const { posts, Post } = require('../models/postModel');
+const { post } = require('../routes/post');
 
 // view all posts logic/endpoint
 const allPost = (req, res) => {
   try {
     return res.status(200).json({ data: posts });
   } catch (error) {
-      console.log(error)
+    console.log(error);
     return res.status(500).json({ message: error.message });
   }
 };
 
 const addPost = (req, res) => {
+  try {
+    const { title, content } = req.body;
+
+    // validating data property
+    if (!title || !content) {
+      return res.status(400).json({ message: 'Bad request' });
+    }
+
+    // create a new post
+    const newPost = new Post(posts.length + 1, title, content);
+    posts.push(newPost);
+    res.status(201).json({
+      status: 'success',
+      message: 'posts created successfully',
+      data: newPost,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const getAPost = (req, res) => {
+  try {
+    const post = posts.find((m) => m.id === parseInt(req.params.id));
+
+    if (!post) {
+      return res
+        .status(404)
+        .json({ success: false, message: 'Post not found' });
+    }
+
+    return res.status(200).json({ success: true, data: post });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+const updatePost = (req, res) => {
+  const post = posts.find((m) => m.id === parseInt(req.params.id));
+  if (!post)
+    return res.status(404).json({ success: false, message: 'Post not found' });
+
   const { title, content } = req.body;
-  const newPost = new Post(posts.length + 1, title, content);
-  posts.push(newPost);
-  res.status(201).json({ success: true, data: newPost });
+  if (title) post.title = title;
+  if (content) post.content = content;
+
+  res.status(200).json({ success: true, data: post });
 };
 
 module.exports = {
   allPost,
   addPost,
+  getAPost,
+  updatePost
 };
--- src/routes/post.js
+++ src/routes/post.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const { allPost, addPost } = require("../controllers/post");
+const { allPost, addPost, getAPost, updatePost } = require("../controllers/post");
 const router = express.Router();
 
 
 router.get("/posts", allPost);
 router.post("/posts", addPost)
+router.get('/posts/:id', getAPost);
+router.put('/posts/:id', updatePost);
+
 
 
 module.exports = router;
